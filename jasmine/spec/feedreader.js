eecc0f39570debbff6d5b50f1674cd2e8dc50aa7
Melanie A <mkkakau@gmail.com>
2016-08-10 16:48:34 -1000

    test: Create test suite for the menu

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -22,8 +22,9 @@ $(function() {
     });
   });
 
+  describe('The menu', function () {
 
-  /* TODO: Write a new test suite named "The menu" */
+  });
 
       /* TODO: Write a test that ensures the menu element is
        * hidden by default. You'll have to analyze the HTML and
